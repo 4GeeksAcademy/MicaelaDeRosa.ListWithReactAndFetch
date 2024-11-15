bed6d7cb91bed1d2d1830ce3346bb5c54eb91422
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-15 23:16:59 +0000

    MicaelaDeRosa.ListWithReactandFetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,63 +6,86 @@ const Home = () => {
 	const [todos, setTodos] = useState([]);
 
 
+	useEffect(() => {
+		getTodos();
+	}, []);
+
 	function postTodos() {
 		let newTask = {
-			label: "Study",
+			label: inputValue,
+			is_done: false,
 		}
 		fetch("https://playground.4geeks.com/todo/todos/MicaelaDeRosa23", {
 			method: "POST",
-			body: JSON.stringify({label : newTask}),
+			body: JSON.stringify(newTask),
 			headers: {
 				"Content-Type": "application/json"
 			}
 		})
-			.then((response) => { response.json })
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setTodos([...todos, data]);
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+
+	function getTodos() {
+		fetch("https://playground.4geeks.com/todo/users/MicaelaDeRosa23")
+			.then((resp) => {
+				console.log(resp.status)
+				return resp.json()
+			})
+			.then((data) => {
+				console.log(data)
+				setTodos(data.todos)
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}
+
+	function deleteTodo(todoId) {
+		const deleteTasks = todos.filter((todo) => todo.id !== todoId)
+		fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+			method: "DELETE",
+			body: JSON.stringify(deleteTasks),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => {
+				if (response.ok) {
+					console.log("Delete Tasks");
+					getTodos();
+				}
+
+			})
 			.then((data) => {
 				console.log(data);
 			})
-			.catch((error) => { return error })
+			.catch((error) => { console.error("Error delete tasks", error); })
 	}
-
-
-	fetch("https://playground.4geeks.com/todo/users/MicaelaDeRosa23")
-		.then((resp) => {
-			console.log(resp.status)
-			return resp.json()
-		})
-		.then((data) => {
-			console.log(data)
-			//setTodos(data.todos)
-		})
-		.catch((error) => { return error })
-
-		function deleteTodos() {
-			let deleteTasks = {
-				label: "Study",
-			}
-			fetch("https://playground.4geeks.com/todo/users/MicaelaDeRosa23", {
-				method: "DELETE",
-				body: JSON.stringify(deleteTasks),
-				headers: {
-					"Content-Type": "application/json"
-				}
-			})
-				.then(response => {if(response.ok){console.log("Delete Tasks");
-				}
-				 })
-				.then((data) => {
-					console.log(data);
-				})
-				.catch((error) => {console.error("Error delete tasks",error );})
-		};
-
 
 	return (
 		<div className="container mt-5">
 			<h1>Todo List</h1>
-			{todos.length == 0 ? <h1>Wait</h1> : todos.map((todo, index, array) => {
+			{todos.length == 0 ? <h1>Wait</h1> : todos.map((todo) => {
 				console.log(todo);
-				return <h1>(new  Task)</h1>
+				return <div className="d-flex">
+
+					<h1 key={todo.id}>{todo.label}</h1>
+					<div onClick={() => { deleteTodo(todo.id) }} >
+						<i
+							class=" fas fa-trash-alt"
+						></i>
+
+
+					</div>
+				</div>
+
 			})}
 			<ul>
 				<li>
@@ -75,12 +98,12 @@ const Home = () => {
 								setTodos(todos.concat([inputValue]));
 								setInputValue("");
 								postTodos();
-								deleteTodos();
+
 							}
 						}}
 						placeholder="What do you need to do?"></input>
 				</li>
-				{todos.map((item, index) => (
+				{/* {todos.map((item, index) => (
 					<li>
 						{item}{""}
 						<i
@@ -94,12 +117,12 @@ const Home = () => {
 								)
 							}></i>
 					</li>
-				))}
+				))} */}
 			</ul>
 			<div>{todos.length} tasks</div>
 		</div>
 	);
-}
+};
 
 
 export default Home;
